fbb6451fb65e06545f80dad96854ccd00c9b6701
davidde9 <david.mteixeira@hotmail.com>
2025-10-09 23:54:15 -0300

    Create Ex.2.cs

---
+++ Relatorio 7/Ex.2.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+public class Pokemon
+{
+    public string Nome { get; set; }
+
+    public Pokemon(string nome)
+    {
+        Nome = nome;
+    }
+
+    public virtual void Atacar()
+    {
+        Console.WriteLine($"{Nome} realiza um ataque genérico!");
+    }
+}
+
+public class PokemonDeFogo : Pokemon
+{
+    public PokemonDeFogo(string nome) : base(nome) { }
+
+    public override void Atacar()
+    {
+        Console.WriteLine($"{Nome} lança uma rajada de fogo!");
+    }
+}
+
+public class PokemonDeAgua : Pokemon
+{
+    public PokemonDeAgua(string nome) : base(nome) { }
+
+    public override void Atacar()
+    {
+        Console.WriteLine($"{Nome} lança um jato de água!");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pokemon> pokemons = new List<Pokemon>();
+
+        Pokemon charizard = new PokemonDeFogo("Charizard");
+        Pokemon blastoise = new PokemonDeAgua("Blastoise");
+
+        pokemons.Add(charizard);
+        pokemons.Add(blastoise);
+
+        foreach (Pokemon p in pokemons)
+        {
+            p.Atacar();
+        }
+    }
+
+}
